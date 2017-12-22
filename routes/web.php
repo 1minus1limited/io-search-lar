3ccf7b60e165651e9c8416c514ddc39dff1d12c7
altescape <hello@michaelwatts.me>
2017-12-22 09:25:14 +0000

    Tidy unused files

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,5 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/playlists', 'IoPlaylistsController@index');
-
 Route::get('/playlist/{id}', 'IoPlaylistsController@playlist');
-
 Route::post('/playlist', 'IoPlaylistsController@create');
